6bf6bdce1d2b10691f5b9d2d0c96dd3ce8a85718
Ruzhang Zhao <rzhao@jhu.edu>
2024-07-24 22:47:32 +0800

    new weight approach

--- R/helper_rcpp.R
+++ R/helper_rcpp.R
@@ -554,19 +554,22 @@ weighted_C_half_func<-function(inv_C_train,weight,dim_U1,dim_U2,tune_weight_meth
     }else if(tune_weight_method == 5){
         diag(inv_C_train)<-diag(inv_C_train)*(weight+1)
     }else if(tune_weight_method == 6){
-        soft_thres<-function(M,weight){
-            off_diag<-M[row(M)!=col(M)]
-            max_off<-max(abs(off_diag))
-            min_off<-min(abs(off_diag))
-            lam<-min_off+(max_off-min_off)*weight
-            diag<-diag(M)
-            M1<-M-lam
-            M2<-M+lam
-            M<-M1*(M1>0)+M2*(M2<0)
-            diag(M)<-diag
-            M
-        }
-        inv_C_train<-soft_thres(inv_C_train,weight)
+        inv_C_train[c(1:dim_U1),c(1:dim_U1)]<-inv_C_train[c(1:dim_U1),c(1:dim_U1)]*(1+weight)
+        inv_C_train[c(1:dim_U1),-c(1:dim_U1)]<-inv_C_train[c(1:dim_U1),-c(1:dim_U1)]*sqrt(1+weight)
+        inv_C_train[-c(1:dim_U1),c(1:dim_U1)]<-inv_C_train[-c(1:dim_U1),c(1:dim_U1)]*sqrt(1+weight)
+        # soft_thres<-function(M,weight){
+        #     off_diag<-M[row(M)!=col(M)]
+        #     max_off<-max(abs(off_diag))
+        #     min_off<-min(abs(off_diag))
+        #     lam<-min_off+(max_off-min_off)*weight
+        #     diag<-diag(M)
+        #     M1<-M-lam
+        #     M2<-M+lam
+        #     M<-M1*(M1>0)+M2*(M2<0)
+        #     diag(M)<-diag
+        #     M
+        # }
+        # inv_C_train<-soft_thres(inv_C_train,weight)
         # inv_C_train2<-inv_C_train[c(1:dim_U1),c(1:dim_U1)]
         # cutoff<-mean(abs(inv_C_train2[row(inv_C_train2)!=col(inv_C_train2)]))
         # inv_C_train2[abs(inv_C_train2)<cutoff]<-0
